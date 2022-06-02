b2196392f197bbfa9432f5dcdac83ad2a59ac343
raja <raja@raja>
2022-06-03 00:48:56 +0530

    all doen

--- app/Http/Livewire/Admin/AdminDashboardComponent.php
+++ app/Http/Livewire/Admin/AdminDashboardComponent.php
@@ -3,11 +3,20 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
-
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 class AdminDashboardComponent extends Component
 {
     public function render()
     {
+
+        $dateS = Carbon::now()->subMonth(3);
+        $dateE = Carbon::now(); 
+        $monthly_total = DB::table('orders')
+        ->whereBetween('placed_at',[$dateS,$dateE])
+     
+        ->sum('total_cost');
+
         return view('livewire.admin.admin-dashboard-component')->layout('layouts.dashboard');
     }
 }
