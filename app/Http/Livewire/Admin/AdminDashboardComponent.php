d6f2fa6e5b8f0c0362c65f10c2b6bd61931c8559
raja <raja@raja>
2022-06-03 22:29:37 +0530

    all done

--- app/Http/Livewire/Admin/AdminDashboardComponent.php
+++ app/Http/Livewire/Admin/AdminDashboardComponent.php
@@ -5,18 +5,54 @@ namespace App\Http\Livewire\Admin;
 use Livewire\Component;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+
 class AdminDashboardComponent extends Component
 {
     public function render()
     {
+        // amount
+        $all_total = 0;
+        $order_amount = 0;
+        $delivered_amount = 0;
+        $cancel_amount = 0;
 
-        $dateS = Carbon::now()->subMonth(3);
-        $dateE = Carbon::now(); 
-        $monthly_total = DB::table('orders')
-        ->whereBetween('placed_at',[$dateS,$dateE])
-     
-        ->sum('total_cost');
+        $all_totals = DB::table('orders')->select('total')->get();
+        foreach ($all_totals as $total) {
+            $all_total = $all_total +  str_replace(",", "", $total->total);
+        }
 
-        return view('livewire.admin.admin-dashboard-component')->layout('layouts.dashboard');
+        $order_amounts = DB::table('orders')->select('total')->where('status', 'delivered')->get();
+        foreach ($order_amounts as $order) {
+            $order_amount = $order_amount +  str_replace(",", "", $order->total);
+        }
+
+        $delivered_amounts = DB::table('orders')->select('total')->where('status', 'delivered')->get();
+        foreach ($delivered_amounts as $delivered) {
+            $delivered_amount = $delivered_amount +  str_replace(",", "", $delivered->total);
+        }
+
+        $cancel_amounts = DB::table('orders')->select('total')->where('status', 'canceled')->get();
+        foreach ($cancel_amounts as $cancel) {
+            $cancel_amount = $cancel_amount +  str_replace(",", "", $cancel->total);
+        }
+
+        $all_total_count = DB::table('orders')->count();
+        $cancel_count = DB::table('orders')->where('status', 'canceled')->count();
+        $order_count = DB::table('orders')->where('status', 'ordered')->count();
+        $delivered_count = DB::table('orders')->where('status', 'delivered')->count();
+
+
+
+
+        return view('livewire.admin.admin-dashboard-component', [
+            'all_total' => $all_total,
+            'order_amount' => $order_amount,
+            'cancel_amount' => $cancel_amount,
+            'delivered_amount' => $delivered_amount,
+            'all_total_count' => $all_total_count,
+            'cancel_count' => $cancel_count,
+            'order_count' => $order_count,
+            'delivered_count' => $delivered_count,
+        ])->layout('layouts.dashboard');
     }
 }
